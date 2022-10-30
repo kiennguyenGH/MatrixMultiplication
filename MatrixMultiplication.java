eb285ea975d924967f0e42d5f3c008b73cb9a89f
kiennguyenGH <ktnguyen4647@gmail.com>
2022-10-30 13:08:38 -0700

    Generate random matrix

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -1,9 +1,36 @@
-
+import java.util.Random;
 public class MatrixMultiplication
 {
 
+    public static int[][] generateMatrix(int size)
+    {
+        Random generator = new Random();
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix[i][j] = (generator.nextInt(size) + 1);
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[i].length; j++)
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args)
     {
-
+        int[][] matrix = generateMatrix(4);
+        printMatrix(matrix);
     }
 }
